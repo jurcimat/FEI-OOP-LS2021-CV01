b500bab2f577a6d355ac0a21bc8839cebbe1bd97
jurcimat <xjurcikm@stuba.sk>
2021-03-25 10:51:11 +0100

    DO WHILE

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -23,6 +23,44 @@ public class App {
         /*boolean pravda = true;
         boolean nepravda = false;*/
 
+        /*if (2 > 1)
+            System.out.println("Hello");
+        else {
+            if (true) {
+                System.out.println("else");
+            }
+        }*/
+        //int a = 5;
+        /*for (int a = 0; a < 6; a++) {
+            switch (a) {
+                case 1:
+                    System.out.println("a je jedna");
+                    break;
+                case 2:
+                    System.out.println("a je dva");
+                    break;
+                case 3:
+                case 4:
+                    System.out.println("a je 3 alebo 4");
+                    break;
+                default:
+                    System.out.println(String.format("a je %d", a));
+            }
+        }*/
+        /*
+        int a = 0;
+        while ( a < 6){
+            System.out.println("fake for a = " + a);
+            a++;
+        }
+        System.out.println();
+        int b = 0;
+        do {
+            b++;
+            System.out.println("fake for b = " + b);
+        } while (b < 6);*/
 
+
+        //static int scitaj(int a )
     }
 }
