f2877723f5a99d9f201d3b8b8423b55c9c932938
jurcimat <xjurcikm@stuba.sk>
2021-03-25 11:08:46 +0100

    Polia

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -3,7 +3,11 @@ package sk.stuba.fei.uim.oop;
 public class App {
     public static void main(String[] args)
     {
-        /*System.out.println("Hello world");
+        /* HELLO WORLD
+        System.out.println("Hello world");
+        /*
+
+        /* DATOVE TYPY
         int celeCislo = 5;
         long celeCisloVacsie = 5L
         float desatinneSinglePrecision = 5.5f;
@@ -11,7 +15,8 @@ public class App {
         char znak = 'a';
         String retazec = "a";*/
 
-        /*int podiel = 2/3; // 0
+        /* DATOVE TYPY - operacie
+        int podiel = 2/3; // 0
         System.out.println(podiel);
 
         int poDeleni = 2%3; // 2
@@ -20,18 +25,23 @@ public class App {
         float podielDesatinny = 2/3.0f; // 2
         System.out.println(podielDesatinny);*/
 
-        /*boolean pravda = true;
+        /* BOOLEAN
+        boolean pravda = true;
         boolean nepravda = false;*/
 
-        /*if (2 > 1)
+        /*IF ELSE
+        if (2 > 1)
             System.out.println("Hello");
         else {
             if (true) {
                 System.out.println("else");
             }
         }*/
-        //int a = 5;
-        /*for (int a = 0; a < 6; a++) {
+
+        /* SWITCH
+        int a = 5;
+
+        for (int a = 0; a < 6; a++) {
             switch (a) {
                 case 1:
                     System.out.println("a je jedna");
@@ -47,7 +57,7 @@ public class App {
                     System.out.println(String.format("a je %d", a));
             }
         }*/
-        /*
+        /* WHILE A WHILE DO
         int a = 0;
         while ( a < 6){
             System.out.println("fake for a = " + a);
@@ -61,8 +71,32 @@ public class App {
         } while (b < 6);*/
         //System.out.println("5 + 7 je" + scitaj(5,7));
 
+        /* POLE
+        System.out.println("Dĺžka: " + args.length);
+        for (int i=0; i < args.length; i++){
+            System.out.println(args[i]);
+        }*/
+
+        /* POLIA - operacie
+        int[] mojePole = new int[10];
+        for (int i = 0; i < mojePole.length; i++){
+            if (i == 0){
+                mojePole[i] = 1;
+                continue;
+            }
+
+            mojePole[i] = mojePole[i - 1] * 2;
+
+        }
+
+        for (int i = 0; i < mojePole.length; i++){
+            System.out.println(i + " : " + mojePole[i]);
+        }*/
+
+
     }
-    /*public static int scitaj(int a, int b){
+    /* FUNKCIA
+    public static int scitaj(int a, int b){
         return a + b;
     }*/
 }
