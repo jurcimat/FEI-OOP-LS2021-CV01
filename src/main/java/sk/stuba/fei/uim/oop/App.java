b09c4d910c39065df7f4b97dd74ba0309eee6fd7
jurcimat <xjurcikm@stuba.sk>
2021-03-25 10:39:09 +0100

    Boolean

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -3,6 +3,26 @@ package sk.stuba.fei.uim.oop;
 public class App {
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+        /*System.out.println("Hello world");
+        int celeCislo = 5;
+        long celeCisloVacsie = 5L
+        float desatinneSinglePrecision = 5.5f;
+        double desatinneDoublePrecision = 5.5;
+        char znak = 'a';
+        String retazec = "a";*/
+
+        /*int podiel = 2/3; // 0
+        System.out.println(podiel);
+
+        int poDeleni = 2%3; // 2
+        System.out.println(poDeleni);
+
+        float podielDesatinny = 2/3.0f; // 2
+        System.out.println(podielDesatinny);*/
+
+        /*boolean pravda = true;
+        boolean nepravda = false;*/
+
+
     }
 }
